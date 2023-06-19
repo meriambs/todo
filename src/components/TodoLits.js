f799f654d5f40ebee56418d82c10014152600fd8
meriambs <bensalah.meriem0@gmail.com>
2023-06-19 12:12:52 +0100

    updated filter element

--- src/components/TodoLits.js
+++ src/components/TodoLits.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoElements from './TodoElements'
 
 
 
 const TodoLits =({todo,handelDelet,checkedElement})  => {
-
+const [sort,setSort]=useState('all')
 console.log('=> ici notre toro ',  todo)
   return (
     <div>   
+
+    <button  onClick={()=>setSort('active')}   >Active</button>
+    <button onClick={()=>setSort('completed')}  >completed</button>
+    <button  onClick={()=>setSort('all')}  >All</button>
+
+<div>
+      {/* display all element */}
+
 {
-  todo.map((e)=>  <TodoElements  minitodo={e} handelDelet={handelDelet} checkedElement={checkedElement} />)
+  todo.length > 0 && sort === "all" ?
+  todo.map((el)=> <TodoElements  minitodo={el} handelDelet={handelDelet} checkedElement={checkedElement}   />   ):(<h1>"there is nothing to show"</h1>)
+}
+
+      {/* display completed element */}
+
+      {
+  todo.length > 0 && sort === "completed" ?
+  todo.filter((el)=> el.checked === true ).map((el)=> <TodoElements  minitodo={el} handelDelet={handelDelet} checkedElement={checkedElement} />):null
 }
 
 
+      {/* display active element */}
+
+      {
+  todo.length > 0 && sort === "active" ?
+  todo.filter((el)=> el.checked === false ).map((el)=> <TodoElements  minitodo={el} handelDelet={handelDelet} checkedElement={checkedElement} />):null
+}
+
+
+
+
+
+
+
+</div>
+
+          
+ 
 </div>
   )
 }
